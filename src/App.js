e6d189290a2ab1f02b9941a4531b945a631b3f85
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-02-25 16:51:14 +0000

    Completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "./Logo";
+import BestPokemon from "./BestPokemon";
+import CaughtPokemon from "./CaughtPokemon";
+
+const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+const date = new Date().toLocaleDateString();
+const App = ()=> {
+  return (
+    <div className="App">
+      <Logo appName="Pokedex" />
+      <BestPokemon pokAbility={abilities} />
+      <CaughtPokemon pokDate= {date} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/BestPokemon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+ const BestPokemon = (props) =>{
+  //const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+return(
+    <div>
+      <p>My favorite Pokemon is Squirtle</p>
+      <ul>
+        {props.pokAbility.map((ability) => (
+          <li key={ability}>{ability}</li>
+        ))}
+      </ul>
+    </div>
+);
+};
+export default BestPokemon;
