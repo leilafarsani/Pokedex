768e6a5c0bec1b68c9b2fc0ecc139b5659755354
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-06 00:34:27 +0000

    Adding Number

--- src/CaughtPokemon.jsx
+++ src/CaughtPokemon.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
 const CaughtPokemon = (props) => {
-  //const date = new Date().toLocaleDateString();
-  return <p> Caught 0 Pokemon on {props.pokDate}</p>;
+ //const date = new Date().toLocaleDateString();
+ const [caught, setCaught] = useState(0);
+  function catchPokemon() {
+    setCaught(caught + 1);
+  }
+ return (
+   <div>
+     <button onClick={catchPokemon}>Catch Pokemon</button>
+     <p>
+       Caught {caught} Pokemon on {props.pokDate}
+     </p>
+     ;
+   </div>
+ );
 };
 export default CaughtPokemon;
