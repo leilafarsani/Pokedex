da7f8e8b69f0468f4a804ce48ce6a71ff5acd0ce
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-07 13:43:22 +0000

    Catch Rndom

--- src/CaughtPokemon.jsx
+++ src/CaughtPokemon.jsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 
 const CaughtPokemon = (props) => {
  //const date = new Date().toLocaleDateString();
- const [caught, setCaught] = useState(0);
+ const [caught, setCaught] = useState([]);
   function catchPokemon() {
-    setCaught(caught + 1);
+    let pokemonArray = ["Eevee", "Ditto", "Squirtle", "Spearow"];
+    setCaught(caught.concat(pokemonArray[Math.floor(Math.random()* pokemonArray.length)]));
   }
  return (
    <div>
      <button onClick={catchPokemon}>Catch Pokemon</button>
      <p>
-       Caught {caught} Pokemon on {props.pokDate}
+       Caught {caught.length} Pokemon on {props.pokDate}
      </p>
-     ;
+     <ul>
+       {caught.map((pokemon, index) => (
+         <li key={index}>{pokemon}</li>
+       ))}
+     </ul>
    </div>
  );
 };
